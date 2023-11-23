b458fcb207d8ec6564f0b8946315c49a6dd295d3
Fauno Guazina <prof.guazina@gmail.com>
2023-11-23 18:43:40 +0100

    adaptation of the logic of use of the music list and button operation

--- AndroidProject/app/src/main/java/com/faunog/m08_act03/MediaPlayerActivity.java
+++ AndroidProject/app/src/main/java/com/faunog/m08_act03/MediaPlayerActivity.java
@@ -1,42 +1,40 @@
 package com.faunog.m08_act03;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
+import java.util.List;
 
-public class MediaPlayerActivity  extends AppCompatActivity {
-    private MediaPlayer mediaPlayer;
+public class MediaPlayerActivity extends AppCompatActivity {
     private Button playPauseButton, stopButton, forwardButton, backwardButton, nextButton, prevButton;
     private SeekBar seekBar;
+    private MediaPlayer mediaPlayer;
+    private List<String> songList;
+    private String songPath;
+    private int currentSongPosition = 0;
 
-    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media_player);
 
         // Inicializa los elementos de la interfaz
-        playPauseButton = findViewById(R.id.playPauseButton);
-        stopButton = findViewById(R.id.stopButton);
-        forwardButton = findViewById(R.id.forwardButton);
-        backwardButton = findViewById(R.id.backwardButton);
-        nextButton = findViewById(R.id.nextButton);
-        prevButton = findViewById(R.id.prevButton);
-        seekBar = findViewById(R.id.seekBar);
+        initializeInterfaceElements();
 
         // Inicializa el reproductor
-        mediaPlayer = new MediaPlayer();
+        initializeMediaPlayerElements();
         try {
-            mediaPlayer.setDataSource("ruta de la canción seleccionada");
+            mediaPlayer.setDataSource(songPath);
             mediaPlayer.prepare();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            Log.e("MediaPlayerActivity", "Error en Try mediaPlayer.setDataSource(songPath):\n" + e.getMessage(), e);
         }
         mediaPlayer.setLooping(true); // Repetir la canción
 
@@ -44,17 +42,17 @@ public class MediaPlayerActivity  extends AppCompatActivity {
         playPauseButton.setOnClickListener(v -> {
             if (mediaPlayer.isPlaying()) {
                 mediaPlayer.pause();
-                playPauseButton.setText("Play");
+                playPauseButton.setText(getString(R.string.activity_media_player_ButtonPlay));
             } else {
                 mediaPlayer.start();
-                playPauseButton.setText("Pause");
+                playPauseButton.setText(getString(R.string.activity_media_player_ButtonPause));
             }
         });
 
         stopButton.setOnClickListener(v -> {
             mediaPlayer.seekTo(0);
             mediaPlayer.pause();
-            playPauseButton.setText("Play");
+            playPauseButton.setText(getString(R.string.activity_media_player_ButtonPlay));
         });
 
         forwardButton.setOnClickListener(v -> {
@@ -68,17 +66,57 @@ public class MediaPlayerActivity  extends AppCompatActivity {
         });
 
         nextButton.setOnClickListener(v -> {
-            // Lógica para reproducir la siguiente canción
+            if (currentSongPosition < songList.size() - 1) {
+                // Hay una siguiente canción en la lista
+                currentSongPosition++;
+
+                try {
+                    mediaPlayer.reset();
+                    mediaPlayer.setDataSource(songList.get(currentSongPosition));
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                    updateSeekBar();
+                } catch (IOException e) {
+                    Log.e("MediaPlayerActivity", "Error al reproducir la siguiente canción:\n" + e.getMessage(), e);
+                }
+            } else {
+                // Estás en la última canción, puedes decidir qué hacer aquí
+                // Por ejemplo, volver al principio o detener la reproducción
+                currentSongPosition = 0;
+                mediaPlayer.stop();
+                mediaPlayer.reset();
+            }
         });
 
         prevButton.setOnClickListener(v -> {
-            // Lógica para reproducir la canción anterior
+            if (currentSongPosition > 0) {
+                // Hay una canción anterior en la lista
+                currentSongPosition--;
+
+                try {
+                    mediaPlayer.reset();
+                    mediaPlayer.setDataSource(songList.get(currentSongPosition));
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                    updateSeekBar();
+                } catch (IOException e) {
+                    Log.e("MediaPlayerActivity", "Error al reproducir la canción anterior:\n" + e.getMessage(), e);
+                }
+            } else {
+                // Estás en la primera canción, puedes decidir qué hacer aquí
+                // Por ejemplo, volver al final o detener la reproducción
+                currentSongPosition = songList.size() - 1;
+                mediaPlayer.stop();
+                mediaPlayer.reset();
+            }
         });
+
 
         // Configura el listener para la barra de progreso
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // Actualiza la posición de reproducción según sea necesario
                 if (fromUser) {
                     mediaPlayer.seekTo(progress);
                 }
@@ -86,15 +124,70 @@ public class MediaPlayerActivity  extends AppCompatActivity {
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
+                // Puedes realizar acciones cuando el usuario comienza a arrastrar la barra
+                // Por ejemplo, pausar la reproducción si está en curso
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                }
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
+                // Puedes realizar acciones cuando el usuario deja de arrastrar la barra
+                // Por ejemplo, reanudar la reproducción si estaba pausada
+                if (!mediaPlayer.isPlaying()) {
+                    mediaPlayer.start();
+                }
             }
         });
 
+
         // Actualiza la barra de progreso de la canción
         updateSeekBar();
+
+        // Configura el listener para el final de la canción
+        mediaPlayer.setOnCompletionListener(mp -> {
+            // Incrementa la posición actual para reproducir la siguiente canción
+            currentSongPosition++;
+
+            // Verifica si llegaste al final de la lista
+            if (currentSongPosition < songList.size()) {
+                // Configura la nueva fuente de datos y comienza la reproducción
+                try {
+                    mediaPlayer.reset();
+                    mediaPlayer.setDataSource(songList.get(currentSongPosition));
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                    updateSeekBar();  // Asegúrate de actualizar la barra de progreso
+                } catch (IOException e) {
+                    Log.e("MediaPlayerActivity", "Error en mediaPlayer.setOnCompletionListener:\n" + e.getMessage(), e);
+                }
+            } else {
+                // Llegaste al final de la lista, puedes decidir qué hacer aquí
+                // Por ejemplo, regresar al principio o detener la reproducción
+                currentSongPosition = 0;
+                mediaPlayer.stop();
+                mediaPlayer.reset();
+            }
+        });
+
+    }
+
+    private void initializeInterfaceElements() {
+        playPauseButton = findViewById(R.id.playPauseButton);
+        stopButton = findViewById(R.id.stopButton);
+        forwardButton = findViewById(R.id.forwardButton);
+        backwardButton = findViewById(R.id.backwardButton);
+        nextButton = findViewById(R.id.nextButton);
+        prevButton = findViewById(R.id.prevButton);
+        seekBar = findViewById(R.id.seekBar);
+    }
+
+    private void initializeMediaPlayerElements() {
+        mediaPlayer = new MediaPlayer();
+        Intent intent = getIntent();
+        songList = intent.getStringArrayListExtra("SONG_LIST");
+        songPath = intent.getStringExtra("SONG_PATH");
     }
 
     // Método para actualizar la barra de progreso
