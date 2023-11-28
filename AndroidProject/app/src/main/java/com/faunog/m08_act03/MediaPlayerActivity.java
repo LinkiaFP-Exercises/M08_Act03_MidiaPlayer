67815a2f11f4d7ff1c7e82734fe9e240ec200da8
Fauno Guazina <prof.guazina@gmail.com>
2023-11-28 11:56:55 +0100

    inflateMediaPlayerCharacteristics method implementation

--- AndroidProject/app/src/main/java/com/faunog/m08_act03/MediaPlayerActivity.java
+++ AndroidProject/app/src/main/java/com/faunog/m08_act03/MediaPlayerActivity.java
@@ -1,11 +1,16 @@
 package com.faunog.m08_act03;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -15,6 +20,8 @@ import java.util.List;
 
 public class MediaPlayerActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    public TextView songTitleTextView, authorTextView, albumTextView;
+    public ImageView albumImageView;
     private Button playPauseButton, stopButton, forwardButton, backwardButton, nextButton, prevButton;
     private SeekBar seekBar;
     private MediaPlayer mediaPlayer;
@@ -76,6 +83,7 @@ public class MediaPlayerActivity extends AppCompatActivity {
                     mediaPlayer.prepare();
                     mediaPlayer.start();
                     updateSeekBar();
+                    inflateMediaPlayerCharacteristics();
                 } catch (IOException e) {
                     Log.e("MediaPlayerActivity", "Error al reproducir la siguiente canción:\n" + e.getMessage(), e);
                 }
@@ -99,6 +107,7 @@ public class MediaPlayerActivity extends AppCompatActivity {
                     mediaPlayer.prepare();
                     mediaPlayer.start();
                     updateSeekBar();
+                    inflateMediaPlayerCharacteristics();
                 } catch (IOException e) {
                     Log.e("MediaPlayerActivity", "Error al reproducir la canción anterior:\n" + e.getMessage(), e);
                 }
@@ -175,6 +184,12 @@ public class MediaPlayerActivity extends AppCompatActivity {
 
     private void initializeInterfaceElements() {
         toolbar = findViewById(R.id.toolbar);
+
+        songTitleTextView = findViewById(R.id.songTitleTextView);
+        authorTextView = findViewById(R.id.authorTextView);
+        albumTextView = findViewById(R.id.albumTitleTextView);
+        albumImageView = findViewById(R.id.albumImageView);
+
         playPauseButton = findViewById(R.id.playPauseButton);
         stopButton = findViewById(R.id.stopButton);
         forwardButton = findViewById(R.id.forwardButton);
@@ -189,6 +204,48 @@ public class MediaPlayerActivity extends AppCompatActivity {
         Intent intent = getIntent();
         songList = intent.getStringArrayListExtra("SONG_LIST");
         songPath = intent.getStringExtra("SONG_PATH");
+
+    /**
+     * @noinspection resource
+     */
+    private void inflateMediaPlayerCharacteristics() {
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        try {
+            String songPath = songList.get(currentSongPosition);
+            retriever.setDataSource(songPath);
+
+            String songTitle = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+            String songAuthor = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+            String songAlbum = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
+            byte[] albumArt = retriever.getEmbeddedPicture();
+
+            if (songTitle == null) {
+                Log.e("SongAdapter", "No se encontraron metadatos para: " + songPath);
+                songTitle = getString(R.string.activity_media_player_songTitleTextView);
+            }
+
+            songTitleTextView.setText(songTitle);
+            authorTextView.setText(songAuthor != null ? songAuthor : getString(R.string.activity_media_player_authorTextView));
+            albumTextView.setText(songAlbum != null ? songAlbum : getString(R.string.activity_media_player_albumTitleTextView));
+
+            if (albumArt != null) {
+                Bitmap bitmap = BitmapFactory.decodeByteArray(albumArt, 0, albumArt.length);
+                albumImageView.setImageBitmap(bitmap);
+            } else {
+                // Mostrar imagen predeterminada
+                int resourceId = R.drawable.unknown_album;
+                albumImageView.setImageResource(resourceId);
+            }
+
+        } catch (Exception e) {
+            Log.e("MediaPlayerActivity", "Error in inflateMediaPlayerCharacteristics:\n" + e.getMessage(), e);
+        } finally {
+            try {
+                retriever.release();
+            } catch (IOException e) {
+                Log.e("MediaPlayerActivity", "Error Trying release retriever:\n" + e.getMessage(), e);
+            }
+        }
     }
 
     // Método para actualizar la barra de progreso
