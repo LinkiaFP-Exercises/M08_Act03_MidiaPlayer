2fa0213d84d233ccf3755dbb7509c8dd993b2277
Fauno Guazina <prof.guazina@gmail.com>
2023-11-29 13:22:47 +0100

    Update SongAdapter.java

--- AndroidProject/app/src/main/java/com/faunog/m08_act03/SongAdapter.java
+++ AndroidProject/app/src/main/java/com/faunog/m08_act03/SongAdapter.java
@@ -17,16 +17,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder> {
-
-    public interface OnItemClickListener {
-        void onItemClick(String songPath);
-    }
-
-    private final List<String> songList;
-    private final Context context;
-    private final OnItemClickListener listener;
 
     public SongAdapter(List<String> songList, Context context, OnItemClickListener listener) {
         this.songList = songList;
@@ -35,25 +28,31 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder> {
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView songTitleTextView;
-        public TextView authorTextView;
-        public TextView albumTextView;
-        public ImageView albumImageView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            initializeInterfaceElements(itemView);
+            itemView.setOnClickListener(this::onClickItenView);
+        }
+
+        private void initializeInterfaceElements(@NonNull View itemView) {
             songTitleTextView = itemView.findViewById(R.id.songTitleTextView);
             authorTextView = itemView.findViewById(R.id.authorTextView);
             albumTextView = itemView.findViewById(R.id.albumTextView);
             albumImageView = itemView.findViewById(R.id.albumImageView);
-
-            itemView.setOnClickListener(v -> {
-                int position = getAdapterPosition();
-                if (position != RecyclerView.NO_POSITION && listener != null) {
-                    listener.onItemClick(songList.get(position));
-                }
-            });
         }
+
+        private void onClickItenView(View view) {
+            int position = getAdapterPosition();
+            if (position != RecyclerView.NO_POSITION && listener != null) {
+                listener.onItemClick(songList.get(position));
+            }
+        }
+
+        public TextView songTitleTextView;
+        public TextView authorTextView;
+        public TextView albumTextView;
+        public ImageView albumImageView;
     }
 
     @NonNull
@@ -61,65 +60,75 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder> {
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Context context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
-
         View songView = inflater.inflate(R.layout.item_song, parent, false);
-
         return new ViewHolder(songView);
     }
-    /** @noinspection resource*/
+
+    /**
+     * @noinspection resource
+     */
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        retriever = new MediaMetadataRetriever();
         String songPath = songList.get(position);
-
-        @SuppressWarnings("ResourceType") MediaMetadataRetriever retriever = new MediaMetadataRetriever();
         File file = new File(songPath);
         if (file.exists()) {
             try {
                 retriever.setDataSource(songPath);
-
-                String songTitle = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
-                String songAuthor = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
-                String songAlbum = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
-                byte[] albumArt = retriever.getEmbeddedPicture();
-
-                if (songTitle == null) {
-                    Log.e("SongAdapter", "No se encontraron metadatos para: " + songPath);
-                    // Mostrar título desconocido
-                    songTitle = context.getString(R.string.activity_media_player_songTitleTextView);
-                }
-
-                // Actualizar la vista con los datos
-                holder.songTitleTextView.setText(songTitle);
-                holder.authorTextView.setText(songAuthor != null ? songAuthor : context.getString(R.string.activity_media_player_authorTextView));
-                holder.albumTextView.setText(songAlbum != null ? songAlbum : context.getString(R.string.activity_media_player_albumTitleTextView));
-
-                if (albumArt != null) {
-                    Bitmap bitmap = BitmapFactory.decodeByteArray(albumArt, 0, albumArt.length);
-                    holder.albumImageView.setImageBitmap(bitmap);
-                } else {
-                    // Mostrar imagen predeterminada
-                    int resourceId = R.drawable.unknown_album;
-                    holder.albumImageView.setImageResource(resourceId);
-                }
+                bindMetadataToViewHolder(holder);
+                bindAlbumArtToViewHolder(holder);
 
             } catch (Exception e) {
-                Log.e("SongAdapter", "Error processing song data:\n" + e.getMessage(), e);
+                Log.e(TAG, "Error processing song data:\n" + e.getMessage(), e);
+
             } finally {
                 try {
                     retriever.release();
                 } catch (IOException e) {
-                    Log.e("SongAdapter", "Error Trying release retriever:\n" + e.getMessage(), e);
+                    Log.e(TAG, "Error Trying release retriever:\n" + e.getMessage(), e);
                 }
             }
         } else {
-            Log.e("SongAdapter", "File does not exist: " + songPath);
+            Log.e(TAG, "File does not exist: " + songPath);
         }
     }
 
+    private void bindMetadataToViewHolder(ViewHolder holder) {
+        String songTitle = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+        String songAuthor = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+        String songAlbum = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
 
+        holder.songTitleTextView.setText(Optional.ofNullable(songTitle)
+                .orElse(context.getString(R.string.activity_media_player_songTitleTextView)));
+        holder.authorTextView.setText(Optional.ofNullable(songAuthor)
+                .orElse(context.getString(R.string.activity_media_player_authorTextView)));
+        holder.albumTextView.setText(Optional.ofNullable(songAlbum)
+                .orElse(context.getString(R.string.activity_media_player_albumTitleTextView)));
+    }
+
+    private void bindAlbumArtToViewHolder(ViewHolder holder) {
+        byte[] albumArt = retriever.getEmbeddedPicture();
+        if (albumArt != null) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(albumArt, 0, albumArt.length);
+            holder.albumImageView.setImageBitmap(bitmap);
+        } else {
+            int resourceId = R.drawable.unknown_album;
+            holder.albumImageView.setImageResource(resourceId);
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(String songPath);
+    }
 
     @Override
     public int getItemCount() {
         return songList.size();
     }
+
+    private final List<String> songList;
+    private final Context context;
+    private final OnItemClickListener listener;
+    private MediaMetadataRetriever retriever;
+    private static final String TAG = "SongAdapter";
 }
